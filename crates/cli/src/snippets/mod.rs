786fb163591fd70074e48eb1ed84a85859e6894f
Henry Corse <kaedraar@gmail.com>
2025-04-15 14:48:40 -0400

    Documentation work

--- crates/cli/src/snippets/mod.rs
+++ crates/cli/src/snippets/mod.rs
@@ -10,3 +10,5 @@ pub const EXAMPLE_TABLE_PROBABILITIES: &str = include_str!("ex03_table_probabili
 pub const EXAMPLE_TABLE_CSV_KEYS: &str = include_str!("ex04_table_csv_keys.tale");
 pub const EXAMPLE_TABLE_LOOKUP: &str = include_str!("ex05_table_lookup.tale");
 pub const EXAMPLE_TABLE_TAGS: &str = include_str!("ex06_table_tags.tale");
+
+pub const EXAMPLE_TABLE_GROUP_BASICS: &str = include_str!("ex11_table_group_basics.tale");
